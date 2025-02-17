d2b968f471e2c23fe1d1663ba2663a57983d76f5
ReshmiRahul <reshmihl94@gmail.com>
2025-02-16 23:14:31 -0500

    Adding the home page

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,6 @@
-import './bootstrap';
+document.addEventListener("DOMContentLoaded", function() {
+    const imageURL = 'http://127.0.0.1:8000/images/img1.png';  // Directly use the image URL
+
+    // Apply background image immediately on load
+    document.querySelector('.hero-section').style.backgroundImage = `url('${imageURL}')`;
+});
--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
 use App\Http\Controllers\MediaController;
+use App\Http\Controllers\HomeController;
 
+Route::get('/', [HomeController::class, 'index']);
 Route::get('/media', [MediaController::class, 'index']);
