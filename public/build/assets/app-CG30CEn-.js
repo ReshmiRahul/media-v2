2da344e32af348c89aeeddae35c0d916bdf77867
ReshmiRahul <reshmihl94@gmail.com>
2025-02-21 11:47:26 -0500

    Removing the build from gitignore

---
+++ public/build/assets/app-CG30CEn-.js
@@ -0,0 +1,1 @@
+document.addEventListener("DOMContentLoaded",function(){const e="http://127.0.0.1:8000/images/img1.png";document.querySelector(".hero-section").style.backgroundImage=`url('${e}')`});
